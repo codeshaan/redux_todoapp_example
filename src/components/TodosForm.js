087ce0f7c04b9eeb844e0800f28013c4682f0b40
Shaan Alam <shaanalam369@gmail.com>
2020-07-08 13:36:51 +0530

    fixed bugs

--- src/components/TodosForm.js
+++ src/components/TodosForm.js
@@ -30,7 +30,17 @@ function TodosForm(props) {
   };
 
   const handleFilter = (filter) => {
-    setFilterTitle(filter);
+    let filterTitle = "";
+    
+    if (filter === visibilityFilters.SHOW_ALL) {
+      filterTitle = 'All';
+    } else if (filter=== visibilityFilters.COMPLETED) {
+      filterTitle = 'Completed'
+    } else {
+      filterTitle = 'Incomplete'
+    }
+
+    setFilterTitle(filterTitle);
     props.filterTodos(visibilityFilters[filter]);
   };
 
@@ -51,16 +61,16 @@ function TodosForm(props) {
                   width="1em"
                   height="1em"
                   viewBox="0 0 16 16"
-                  class="bi bi-clipboard-check"
+                  className="bi bi-clipboard-check"
                   fill="currentColor"
                   xmlns="http://www.w3.org/2000/svg"
                 >
                   <path
-                    fill-rule="evenodd"
+                    fillRule="evenodd"
                     d="M4 1.5H3a2 2 0 0 0-2 2V14a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2V3.5a2 2 0 0 0-2-2h-1v1h1a1 1 0 0 1 1 1V14a1 1 0 0 1-1 1H3a1 1 0 0 1-1-1V3.5a1 1 0 0 1 1-1h1v-1z"
                   />
                   <path
-                    fill-rule="evenodd"
+                    fillRule="evenodd"
                     d="M9.5 1h-3a.5.5 0 0 0-.5.5v1a.5.5 0 0 0 .5.5h3a.5.5 0 0 0 .5-.5v-1a.5.5 0 0 0-.5-.5zm-3-1A1.5 1.5 0 0 0 5 1.5v1A1.5 1.5 0 0 0 6.5 4h3A1.5 1.5 0 0 0 11 2.5v-1A1.5 1.5 0 0 0 9.5 0h-3zm4.354 7.146a.5.5 0 0 1 0 .708l-3 3a.5.5 0 0 1-.708 0l-1.5-1.5a.5.5 0 1 1 .708-.708L7.5 9.793l2.646-2.647a.5.5 0 0 1 .708 0z"
                   />
                 </svg>
@@ -68,8 +78,8 @@ function TodosForm(props) {
             </InputGroup>
           </Col>
           <Col md="4" sm="4" xs="24">
-            <Button appearance="primary" block>
-              <Icon icon="plus" size="xs" /> Add
+            <Button appearance="primary">
+              <Icon icon="plus" /> Add
             </Button>
           </Col>
           <Col md="4" sm="6" xs="24">
@@ -80,19 +90,19 @@ function TodosForm(props) {
             >
               <Dropdown.Item
                 icon={<Icon icon="globe" />}
-                onSelect={() => handleFilter("All")}
+                onSelect={() => handleFilter(visibilityFilters.SHOW_ALL)}
               >
                 Show All
               </Dropdown.Item>
               <Dropdown.Item
                 icon={<Icon icon="check" />}
-                onSelect={() => handleFilter("Completed")}
+                onSelect={() => handleFilter(visibilityFilters.COMPLETED)}
               >
                 Show Completed
               </Dropdown.Item>
               <Dropdown.Item
                 icon={<Icon icon="close" />}
-                onSelect={() => handleFilter("Incomplete")}
+                onSelect={() => handleFilter(visibilityFilters.INCOMPLETE)}
               >
                 Show Incomplete
               </Dropdown.Item>
