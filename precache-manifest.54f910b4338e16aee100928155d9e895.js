01793b5e89a8e2728ad8babc1fd1e0c55322901d
Shaan Alam <shaanalam369@gmail.com>
2020-07-08 12:05:55 +0530

    Updates

---
+++ precache-manifest.54f910b4338e16aee100928155d9e895.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d277ab6f9f94c5c04bcd11dd48f2a20c",
+    "url": "/redux_todoapp_example/index.html"
+  },
+  {
+    "revision": "7629e4e9a8cc65ba198d",
+    "url": "/redux_todoapp_example/static/css/2.f005c5ff.chunk.css"
+  },
+  {
+    "revision": "b058eefab0a197699c18",
+    "url": "/redux_todoapp_example/static/css/main.70a00e1c.chunk.css"
+  },
+  {
+    "revision": "7629e4e9a8cc65ba198d",
+    "url": "/redux_todoapp_example/static/js/2.d6653011.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/redux_todoapp_example/static/js/2.d6653011.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b058eefab0a197699c18",
+    "url": "/redux_todoapp_example/static/js/main.3b44fd62.chunk.js"
+  },
+  {
+    "revision": "0e88d10228f9f92abad9",
+    "url": "/redux_todoapp_example/static/js/runtime-main.c8d6b064.js"
+  }
+]);
